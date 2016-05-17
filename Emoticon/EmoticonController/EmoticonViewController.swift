6e019772ff1d5c31e6482d4bc18d3f9fb4477b1d
Imanol <jk199054@163.com>
2016-05-17 16:18:34 +0800

    完善collection布局，添加资源文件

--- Emoticon/EmoticonController/EmoticonViewController.swift
+++ Emoticon/EmoticonController/EmoticonViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+let customCollectionViewCellIdentifer = "customCollectionViewCellIdentifer"
 class EmoticonViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,15 +17,15 @@ class EmoticonViewController: UIViewController {
     }
 
     func setupUI(){
-        view.addSubview(collection)
+        view.addSubview(collectionV)
         view.addSubview(toolBar)
         
         //layout
-        collection.translatesAutoresizingMaskIntoConstraints = false
+        collectionV.translatesAutoresizingMaskIntoConstraints = false
         toolBar.translatesAutoresizingMaskIntoConstraints = false
         
         var cons = [NSLayoutConstraint]()
-        let dict = ["collectionVeiw": collection, "toolbar": toolBar]
+        let dict = ["collectionVeiw": collectionV, "toolbar": toolBar]
         cons += NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[collectionVeiw]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
         cons += NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[toolbar]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
         cons += NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[collectionVeiw]-[toolbar(44)]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
@@ -39,7 +40,13 @@ class EmoticonViewController: UIViewController {
     }
     
     //Mark: - lazyLoading
-    private lazy var collection : UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
+    private lazy var collectionV : UICollectionView = {
+        let collection = UICollectionView(frame: CGRectZero, collectionViewLayout: CustomFlowLayout())
+        collection.registerClass(CustomCollectionViewCell.self, forCellWithReuseIdentifier: customCollectionViewCellIdentifer)
+        collection.dataSource = self
+        collection.delegate = self
+        return collection
+    }()
     
     private lazy var toolBar : UIToolbar = {
         let toolbar = UIToolbar()
@@ -59,4 +66,64 @@ class EmoticonViewController: UIViewController {
         toolbar.tintColor = UIColor.blackColor()
         return toolbar
     }()
+}
+
+extension EmoticonViewController:UICollectionViewDelegate,UICollectionViewDataSource
+{
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(customCollectionViewCellIdentifer, forIndexPath: indexPath) as! CustomCollectionViewCell
+        cell.backgroundColor = (indexPath.item % 2 == 0) ? UIColor.redColor() : UIColor.greenColor()
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 21 * 4
+    }
+}
+
+//Mark: -CustomCollectionViewCell
+class CustomCollectionViewCell : UICollectionViewCell
+{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    func setupUI(){
+        contentView.addSubview(iconButton)
+        iconButton.backgroundColor = UIColor.whiteColor()
+        iconButton.frame = CGRectInset(contentView.bounds, 4, 4)
+    }
+
+    private lazy var iconButton: UIButton = UIButton()
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+//Mark: -CustomFlowLayout
+class CustomFlowLayout : UICollectionViewFlowLayout
+{
+    override func prepareLayout() {
+        super.prepareLayout()
+        //设置cell相关属性
+        let width = (collectionView?.bounds.width)! / 7
+        itemSize = CGSize(width: width, height: width)
+        minimumInteritemSpacing = 0
+        minimumLineSpacing = 0
+        scrollDirection = UICollectionViewScrollDirection.Horizontal
+        
+        // 设置collectionview相关属性
+        collectionView?.pagingEnabled = true
+        collectionView?.bounces = false
+        collectionView?.showsHorizontalScrollIndicator = false
+        
+        //以下设置是为了让item 距离顶部和底部出现间隙，然后把中间原本的间隙挤掉
+        // 注意:最好不要乘以0.5, 因为CGFloat不准确, 所以如果乘以0.5在iPhone4/4身上会有问题
+        let y = (collectionView!.bounds.height - 3*width) * 0.45
+        collectionView?.contentInset = UIEdgeInsetsMake(y, 0, y, 0)
+    }
 }
